558529a5fd8d25e8d744d46f4b5bb3e6abe3a460
rohan.iyer1999@gmail.com <rohan.iyer1999@gmail.com>
2015-09-08 13:51:58 -0700

    fixed documentation builder error after restructuring

--- docs/documentation.js
+++ docs/documentation.js
@@ -1,10 +1,24 @@
 fs = require('fs');
-fs.readFile('top.md', 'utf8', function(err, data1){
-	fs.readFile('docs.md', 'utf8', function(err, data2){
-		fs.readFile('bottom.md', 'utf8', function(err, data3){
-			fs.writeFile('../README.md', data1+data2+data3, function(error){
+fs.readFile('docs/top.md', 'utf8', function(err1, data1){
+	fs.readFile('docs/docs.md', 'utf8', function(err2, data2){
+		fs.readFile('docs/bottom.md', 'utf8', function(err3, data3){
+			fs.writeFile('README.md', data1+data2+data3, function(error){
 				if(error){
+					console.log("an error")
 					return console.log(error);
+
+				}
+				if(err1){
+					console.log("an error")
+					//return console.log(err1);
+				}
+				if(err2){
+					console.log("an error")
+					//return console.log(err2);
+				}
+				if(err3){
+					console.log("an error")
+					//return console.log(err3);
 				}
 
 			})
