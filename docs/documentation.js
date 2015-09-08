b6a8fd204d4b25bf60d57203e83bebb33991db93
rohan.iyer1999@gmail.com <rohan.iyer1999@gmail.com>
2015-09-08 13:35:52 -0700

    restructured repo

--- docs/documentation.js
+++ docs/documentation.js
@@ -2,7 +2,7 @@ fs = require('fs');
 fs.readFile('top.md', 'utf8', function(err, data1){
 	fs.readFile('docs.md', 'utf8', function(err, data2){
 		fs.readFile('bottom.md', 'utf8', function(err, data3){
-			fs.writeFile('README.md', data1+data2+data3, function(error){
+			fs.writeFile('../README.md', data1+data2+data3, function(error){
 				if(error){
 					return console.log(error);
 				}
